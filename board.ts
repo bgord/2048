2c551bcdfa7c2bc9b0d949496457c8d8cc56e6ab
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-05-18 16:11:42 +0200

    refactor: add board class

---
+++ board.ts
@@ -0,0 +1,94 @@
+import { random } from "lodash";
+
+import { Tile } from "./tile";
+
+type BoardStateType = [
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile,
+  Tile
+];
+
+export class Board {
+  state: BoardStateType;
+
+  constructor(state: BoardStateType = Board.getEmpty()) {
+    this.state = state;
+    this.spawnRandomTile();
+  }
+
+  static getEmpty(): BoardStateType {
+    return [
+      new Tile(1),
+      new Tile(2),
+      new Tile(3),
+      new Tile(4),
+      new Tile(5),
+      new Tile(6),
+      new Tile(7),
+      new Tile(8),
+      new Tile(9),
+      new Tile(10),
+      new Tile(11),
+      new Tile(12),
+      new Tile(13),
+      new Tile(14),
+      new Tile(15),
+      new Tile(16),
+    ];
+  }
+
+  spawnRandomTile(): Board {
+    const randomEmptyTileId = this.getRandomEmptyTileId();
+
+    for (const tile of this.state) {
+      if (tile.id === randomEmptyTileId) {
+        tile.setValue(2);
+      }
+    }
+
+    return this;
+  }
+
+  private getRandomEmptyTileId(): Tile["id"] | null {
+    const emptyTiles = this.state.filter(Tile.isEmpty);
+
+    if (emptyTiles.length === 0) return null;
+
+    const randomEmptyTileIndex = random(emptyTiles.length - 1);
+    return emptyTiles[randomEmptyTileIndex].id;
+  }
+
+  private getColumns() {
+    const columns: Tile[][] = [[], [], [], []];
+
+    for (const tile of this.state) {
+      if ([1, 5, 9, 13].includes(tile.id)) {
+        columns[0].push(tile);
+      }
+      if ([2, 6, 10, 14].includes(tile.id)) {
+        columns[1].push(tile);
+      }
+      if ([3, 7, 11, 15].includes(tile.id)) {
+        columns[2].push(tile);
+      }
+      if ([4, 8, 12, 16].includes(tile.id)) {
+        columns[3].push(tile);
+      }
+    }
+
+    return columns;
+  }
+}
--- game.ts
+++ game.ts
@@ -1,113 +1,37 @@
 import { random } from "lodash";
 import { Tile } from "./tile";
+import { Board } from "./board";
 
 export type MoveType = "ArrowUp" | "ArrowRight" | "ArrowDown" | "ArrowLeft";
 
-export type BoardType = [
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile,
-  Tile
-];
-
 export class Game {
-  static emptyBoard: BoardType = [
-    new Tile(1),
-    new Tile(2),
-    new Tile(3),
-    new Tile(4),
-    new Tile(5),
-    new Tile(6),
-    new Tile(7),
-    new Tile(8),
-    new Tile(9),
-    new Tile(10),
-    new Tile(11),
-    new Tile(12),
-    new Tile(13),
-    new Tile(14),
-    new Tile(15),
-    new Tile(16),
-  ];
-
   static defaultScore = 0;
 
   static isProperMove(value: string): value is MoveType {
     return ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"].includes(value);
   }
 
-  static initializeBoard(): BoardType {
-    const board = Array.from(Game.emptyBoard) as BoardType;
-
-    return Game.spawnRandomTile(board);
+  static initialize(): Board {
+    return new Board();
   }
 
-  static handleMove(board: BoardType, type: MoveType): BoardType {
-    return this.spawnRandomTile(board);
+  static handleMove(board: Board, type: MoveType): Board {
+    return board.spawnRandomTile();
   }
 
-  static hasGameEnded(board: BoardType): boolean {
-    return board.every(Tile.hasValue);
+  static hasGameEnded(board: Board): boolean {
+    return board.state.every(Tile.hasValue);
   }
 
-  static getScore(board: BoardType) {
+  static getScore(board: Board) {
     let score = Game.defaultScore;
 
-    for (const tile of board) {
+    for (const tile of board.state) {
       if (tile?.value && tile.value > score) {
         score = tile.value;
       }
     }
 
     return score;
-  }
-
-  private static getColumns(board: BoardType) {
-    const columns: Tile[][] = [[], [], [], []];
-
-    for (const tile of board) {
-      if ([1, 5, 9, 13].includes(tile.id)) {
-        columns[0].push(tile);
-      }
-      if ([2, 6, 10, 14].includes(tile.id)) {
-        columns[1].push(tile);
-      }
-      if ([3, 7, 11, 15].includes(tile.id)) {
-        columns[2].push(tile);
-      }
-      if ([4, 8, 12, 16].includes(tile.id)) {
-        columns[3].push(tile);
-      }
-    }
-
-    return columns;
-  }
-
-  private static spawnRandomTile(board: BoardType): BoardType {
-    const randomEmptyTileId = Game.getRandomEmptyTileId(board);
-
-    return board.map((tile) =>
-      tile.id === randomEmptyTileId ? { ...tile, value: 2 } : tile
-    ) as BoardType;
-  }
-
-  private static getRandomEmptyTileId(board: BoardType): Tile["id"] {
-    const emptyTiles = board.filter(Tile.isEmpty);
-
-    const randomEmptyTileIndex = random(emptyTiles.length - 1);
-
-    return emptyTiles[randomEmptyTileIndex].id;
   }
 }
